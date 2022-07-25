721b8d473ec2f9c29635a8f1b29c7d8e186f386c
0xelia <elia.vanon@yahoo.com>
2022-07-25 20:14:39 +0200

    bonus 1

---
+++ bonus/js/script.js
@@ -0,0 +1,9 @@
+let numbers = [11, 22, 32, 43, 56, 21, 53, 75, 54]
+let lastNumber = (numbers.length - 1)
+let numberStop = (numbers.length - 5)
+
+
+for (let i = lastNumber; i >= numberStop; i--) {
+    console.log(numbers[i])
+} 
+
