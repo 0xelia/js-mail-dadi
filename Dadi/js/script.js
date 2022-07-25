ff4757f4868c7b26855afa95b4bcbcf902fbe2ce
0xelia <elia.vanon@yahoo.com>
2022-07-25 18:59:42 +0200

    js added

--- Dadi/js/script.js
+++ Dadi/js/script.js
@@ -1,1 +1,18 @@
-console.log('ciao')
+
+let submitElement = document.querySelector('input[name="play"]')
+let playerNumber = 0
+let botNumber = 0
+
+submitElement.addEventListener('click', function() {
+    playerNumber = (Math.floor(Math.random()*6) + 1)
+    botNumber = (Math.floor(Math.random()*6) + 1)
+
+    console.log( 'Il tuo numero --> ' + playerNumber, 'IL numero del bot --> ' + botNumber)
+    if (playerNumber > botNumber) {
+        console.log('Complimenti! Hai vinto!')
+    } else if (playerNumber < botNumber) {
+        console.log('Peccato! Ha vinto il bot')
+    } else {
+        console.log('Pareggio! Riprova')
+    }
+})
