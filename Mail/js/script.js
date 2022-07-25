142c40e6fd884a1d041cb295fd98b56d569f8f89
0xelia <elia.vanon@yahoo.com>
2022-07-25 16:42:35 +0200

    ex 1 - js added

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -1,1 +1,24 @@
-// chiedere all'utente la mail
+
+const mails = ['elia@gmail.com', 'luca@gmail.com', 'milio@gmail.com']
+
+let emailElement = document.querySelector('input[name="email"]')
+
+let submitelement = document.querySelector('input[name="checker"]')
+let emaillUser = ''
+
+submitelement.addEventListener('click', function() { 
+    emaillUser = emailElement.value
+    
+    if (emaillUser === mails[0] || emaillUser === mails[1] || emaillUser === mails[2]) {
+        console.log('Accesso riuscito')
+    } else {
+        console.log('Accesso negato')
+    }
+
+    
+    
+})
+
+//verificare che la mail sia nell'arrey dei registrati
+
+// stamapre l'esito dell'operazione
