404b8b77b08e2ca73d69c2cf20d4afd2765b9c46
0xelia <elia.vanon@yahoo.com>
2022-07-25 22:11:21 +0200

    js added

--- bonus/bonus del bonus/js/script.js
+++ bonus/bonus del bonus/js/script.js
@@ -1,10 +1,46 @@
-let numbers = [11, 22, 32, 43, 56, 21, 53, 75, 54]
-let lastNumber = (numbers.length - 1)
-let numberStop = (numbers.length - 5)
+//chiedere il numero di elementi dell array
 
-console.log('valore indice ultimo elemento: ' + lastNumber)
+//generare tanti numeri random quanti il numero richiesto dall'utente
 
-for (let i = lastNumber; i >= numberStop; i--) {
-    console.log(numbers[i])
-} 
+//restituire gli ultimi 5 elementi
+
+let numberElement = document.querySelector('input[name="number"]')
+let extractElement = document.querySelector('input[name="exstract"]')
+
+
+let number = 0
+
+let sumbitElement = document.querySelector('input[type="submit"]')
+sumbitElement.addEventListener('click', function() {
+
+    number = numberElement.value
+    let arrayLength = (parseInt(number))
+    let numberStop = extractElement.value
+
+    if (arrayLength <= 0) {
+        alert('Inserire un numero superiore ad 0')
+        numberElement.value = ''
+    }
+
+    let array = []
+
+    for (i = arrayLength; i > 0; i-- ) {
+        array.push(Math.floor(Math.random(i)*100))
+    }
+
+    console.log(array)
+})
+
+
+
+
+
+// let lastNumber = (numbers.length - 1)
+// let numberStop = (numbers.length - 5)
+
+// console.log('valore indice ultimo elemento: ' + lastNumber)
+
+// for (let i = lastNumber; i >= numberStop; i--) {
+//     console.log(numbers[i])
+// } 
 
